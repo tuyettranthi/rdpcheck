100518e2d2d497d2d7f617b95d5cbfdb136984c0
tuyettranthi <78965369+tuyettranthi@users.noreply.github.com>
2024-01-20 16:12:58 +0700

    Create rdp.py

---
+++ rdp.py
@@ -0,0 +1,21 @@
+import subprocess
+
+def join_vps_rdp(ip_address, username, password):
+    cmd = f"xfreerdp /u:{username} /p:{password} /v:{ip_address}"
+    process = subprocess.Popen(cmd, shell=True)
+    process.wait()
+
+def change_vps_rdp_password(ip_address, username, old_password, new_password):
+    # Run command to change password using net user command
+    cmd = f"net user {username} {new_password}"
+    process = subprocess.Popen(f"xfreerdp /u:{username} /p:{old_password} /v:{ip_address} /cert-ignore && {cmd}", shell=True)
+    process.wait()
+
+# Usage example
+ip_address = "your_ip_address"
+username = "your_username"
+password = "your_password"
+new_password = "adomanbg@123"
+
+join_vps_rdp(ip_address, username, password)
+change_vps_rdp_password(ip_address, username, password, new_password)
