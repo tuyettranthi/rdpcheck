03d709f784a4368857976576fdb590a1b8349295
tuyettranthi <78965369+tuyettranthi@users.noreply.github.com>
2024-01-26 17:29:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ rm -rf ngrok.tgz
 sudo apt install qemu-kvm -y
 
 # Create qemu image
-qemu-img create -f raw win.iso 512G
+qemu-img create -f raw win.img 512G
 
 # Run qemu virtual machine
 qemu-system-x86_64 -m 9G -cpu core2duo -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=24,threads=8 -device e1000,netdev=n0 -netdev user,id=n0 -vga vmware
